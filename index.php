cf7e1d844aeaa5935783a4ea5f762b6c4e2e070f
Olav Schettler <olav.schettler@cocomore.com>
2013-12-16 16:25:48 +0100

    Initial setup

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+ini_set('display_errors', TRUE);
+error_reporting(-1);
+
+date_default_timezone_set('Europe/Berlin');
+
+session_set_cookie_params(86400 * 7);
+session_start();
+
+require_once 'vendor/autoload.php';
+
+ORM::configure('sqlite:db/entangle.sqlite');
+
+config('dispatch.views', './views');
+config('source', 'settings.ini');
+
+on('GET', '/', function () {
+  render('index', array(
+    'site_name' => config('site.name'),
+    'page_title' => config('site.title'),
+  ));
+});
---
+++ views/index.html.php
@@ -0,0 +1,4 @@
+<div class="jumbotron">
+  <h1><?php echo $page_title; ?></h1>
+  <p>Entangle!</p>
+</div>

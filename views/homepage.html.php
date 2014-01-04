6bfe0b2c325f56b000fea4b6d812a03c26b49776
Olav Schettler <olav.schettler@cocomore.com>
2014-01-04 21:21:34 +0100

    Mentioned Github

--- views/homepage.html.php
+++ views/homepage.html.php
@@ -4,6 +4,7 @@ stack('footer', partial('footer_login'));
 <div class="jumbotron">
   <h1><?php echo $page_title; ?></h1>
   <p>Federated timelines. Open source.</p>
-  <p>Browse public timelines or login to manage your own and entangle them with others.   
+  <p>Browse public timelines or login to manage your own and entangle them with others.</p>
+  <p>If you like what you see, why not <a href="https://github.com/oschettler/entangle#entangle">try it at home</a>. It's free and easy.</p>
 </div>
 
